8adabd32f6f5b29f77f277c6c78b86d080baffb2
Pedro Syd <pedro.saadi.aquino@gmail.com>
2016-12-21 17:58:13 -0200

    basic API structure

--- server.js
+++ server.js
@@ -18,6 +18,49 @@ client.on('error', function (err) {
 app.set('port', (process.env.PORT || 5000));
 
 
+//  Fetches a single dev
+app.get('/api/dev/:gitlogin', function(req, res) {
+  const { gitlogin }  = req.params;
+  res.send(`fetched dev ${gitlogin}`);
+});
+
+// Fetches 8 devs from an organizaiton
+app.get('/api/devs/:organization/:pagination', function(req, res) {
+  const { organization, pagination } = req.params;
+  res.send(`fetched devs from ${organization} after pagination ${pagination}`);
+});
+
+// Fetches shopping cart NEEDs AUTH
+app.get('/api/cart/:userid', function(req, res) {
+  const { userid } = req.params;
+  res.send(`fetched cart for ${userid}`);
+});
+
+// Adds dev to shopping cart NEEDs AUTH
+app.post('/api/cart/add/:gitlogin/:userid', function(req, res) {
+  const { gitlogin, userid } = req.params;
+  res.send(`added ${gitlogin} to ${userid}  cart`);
+});
+
+// Deletes dev on a shopping cart NEEDs AUTH
+app.delete('/api/cart/delete/:gitlogin/:userid', function(req, res) {
+  const { gitlogin, userid } = req.params;
+  res.send(`deleted ${gitlogin} from ${userid} cart`);
+});
+
+// Updates hours of a user in a shopping cart NEEDs AUTH
+app.put('/api/cart/edit/hours/:gitlogin/:hours/:userid', function(req, res) {
+  const { gitlogin, hours, userid } = req.params;
+  res.send(`updated  ${gitlogin} hours to ${hours} on ${userid} cart`);
+});
+
+// Applies coupon to a shopping cart NEEDs AUTH
+app.post('/api/cart/apply/coupon/:code/:userid', function(req, res) {
+  const { code, userid } = req.params;
+  res.send(`applied coupon ${code} to ${userid}`);
+});
+
+
 app.listen(app.get('port'), function(){
   console.log('Server listening on port: ', app.get('port'));
 });
